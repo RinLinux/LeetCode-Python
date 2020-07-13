6ac456ab14d630a316ce65057e23c3d0aeeabe9a
Lynzheng <RinLinux@users.noreply.github.com>
2020-07-13 16:17:34 +0800

    modified .py

--- climbStairs.py
+++ climbStairs.py
@@ -21,5 +21,5 @@ class Solution:
         return all_s
 
 s = Solution()
-print(s.climbStairs(4))
+print(s.climbStairs(10))
 
--- insert_sort.py
+++ insert_sort.py
@@ -5,15 +5,15 @@ date: 2020/05/30/00/05
 
 """
 
-def insert_sort(list):
 
+def insert_sort(list):
     for j in range(1, len(list)):
         key = list[j]
         i = j - 1
         while i >= 0 and list[i] > key:
-            list[i+1] = list[i]
+            list[i + 1] = list[i]
             i = i - 1
-        list[i+1] = key
+        list[i + 1] = key
 
     return list
 
