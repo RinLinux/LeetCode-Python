4f41986c3ee5b41beb9f28874439ea1d01dfc9e9
RinLinux <759967211@qq.com>
2024-01-05 15:28:05 +0800

    modified *.py

--- recursion.py
+++ recursion.py
@@ -5,13 +5,13 @@ date: 2020/03/08/12/49
 
 """
 
+
 def product_multiply(x, y):
     if y == 0:
         return 0
     else:
-        return x + product_multiply(x, y-1)
-
+        return x + product_multiply(x, y - 1)
 
 
 if __name__ == '__main__':
-    print(product_multiply(5,3))
+    print(product_multiply(5, 3))
